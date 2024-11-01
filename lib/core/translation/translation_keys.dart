d46170c54ef1b63b5b461afb8770271b1d3c48fc
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-11-01 11:35:40 -0300

    passagem de tradução via enum de tradução e utilitarios OTIMOS

--- lib/core/translation/translation_keys.dart
+++ lib/core/translation/translation_keys.dart
@@ -1,15 +1,64 @@
 import 'package:get/get.dart';
 
+enum TranslateKey {
+  titleApp('title_app'),
+  welcome('welcome'),
+  userListTitleSimple('user_list_title_simple',
+      plural: 'user_list_title_simple_plural'),
+  userListTitle('user_list_title', plural: 'user_list_title_plural');
+
+  final String key;
+  final String? plural;
+
+  const TranslateKey(this.key, {this.plural});
+
+  String get tr => key.tr;
+  //ou assim
+  // String get tr {
+  //   return key.tr;
+  // }
+  String trParams(Map<String, String> params) {
+    return key.trParams(params);
+  }
+
+  String trPlural(int total) {
+    if (plural != null) {
+      return key.trPlural(plural, total);
+    }
+    return '';
+  }
+
+   String trPluralParams(int total, Map<String, String> params) {
+    if (plural != null) {
+      return key.trPluralParams(plural, total, params);
+    }
+    return '';
+  }
+}
+
 class TranslationKeys extends Translations {
   @override
   // Map<String, Map<String, String>> get keys => throw UnimplementedError();
   //criação de chave de tradução
   Map<String, Map<String, String>> get keys => {
         'en_US': {
-          'title_app': 'GetX Tranlate',
+          // 'title_app': 'GetX Tranlate',
+          TranslateKey.titleApp.key: 'GetX Tranlate',
+          //!ACELERA EXECUÇÂO 'welcome': 'Hello @name',
+          TranslateKey.welcome.key: 'Hello @name',
+          'welcome': 'Hello @name',
+          'user_list_title_simple': 'User',
+          'user_list_title_simple_plural': 'Users',
+          'user_list_title': '@total User',
+          'user_list_title_plural': '@total Users'
         },
         'pt_BR': {
-          'title_app': 'GetX Trandução',
+          TranslateKey.titleApp.key: 'GetX Trandução',
+          'welcome': 'Olá @name',
+          'user_list_title_simple': 'Usuário',
+          'user_list_title_simple_plural': 'Usuários',
+          'user_list_title': '@total Usuário',
+          'user_list_title_plural': '@total Usuários',
         }
       };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_translate/core/translation/translation_keys.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,9 +10,47 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
 //!abrir a aba de translations_kes ao lado para entender melhor
-        title: Text('title_app'.tr),
+        // title: Text('title_app'.tr),
+        // title: Text(TranslateKey.titleApp.tr()), //forma 1 para ser SEM função precisa ser GET
+        title: Text(TranslateKey.titleApp.tr), //forma 2
       ),
-      body: Container(),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Text('Welcome'.tr),
+          //! ACELERAÇÂO VIA ENUM
+          //! Text('welcome'.trParams({'name': 'FULANO'})),
+          Text(
+              TranslateKey.titleApp.key.trParams({'name': 'FULANO'})), //forma 1
+          Text(TranslateKey.titleApp.trParams({'name': 'FULANO'})), //forma 2
+
+          // Text('user_list_title'.trPluralParams('user_list_title_plural', 1, {'total': '1'})),//forma 1
+          Text(TranslateKey.userListTitleSimple.trPlural(1)), //forma2
+          // Text('user_list_title'.trPluralParams('user_list_title_plural', 2, {'total': '2'})),//forma 1
+          Text(TranslateKey.userListTitleSimple.trPlural(2)), //forma 2
+          // Text('user_list_title_simple'.trPluralParams('user_list_title_simple_plural', 1)),//forma 1
+          Text(TranslateKey.userListTitle.trPluralParams(1, {'total': '1'})), //forma 2
+          // Text('user_list_title_simple'.trPluralParams('user_list_title_simple_plural', 2)),//forma 1
+          Text(TranslateKey.userListTitle.trPluralParams(2, {'total': '2'})), //forma 2
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Get.updateLocale(const Locale('en', 'US'));
+                  },
+                  child: const Text('Alterar US')),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.updateLocale(const Locale('pt', 'BR'));
+                  },
+                  child: const Text('Alterar BR')),
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
